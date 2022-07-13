2a14d241ad274678379701b256996c607c1dafdf
hybridx <hybridx@gmail.com>
2022-07-14 00:16:21 +0530

    Added handleDateClickEvent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { FullCalendarModule } from '@fullcalendar/angular'; 
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
 
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  interactionPlugin,
+]); // register FullCalendar plugins
 
 @NgModule({
   declarations: [
@@ -16,6 +22,7 @@ import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FullCalendarModule
   ],
   providers: [],
--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,34 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions, FullCalendarModule } from '@fullcalendar/angular'; // useful for typechecking
-import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
-//import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
-
-
-FullCalendarModule.registerPlugins([ // register FullCalendar plugins
-  dayGridPlugin,
-]);
-
+import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    weekends: false // initial value
+    weekends: false, // initial value
+    selectable: true,
+    dateClick: this.handleDateClickEvent.bind(this),
+    events: [
+      {
+        title: 'Event Now',
+        start: '2022-07-15',
+        end: '2022-07-16'
+      }
+    ]
   };
-
 
   toggleWeekends() {
     this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
   }
-  
 
+  addEvent(dateStr: any) {
+    if (this.calendarOptions?.events) {
+      (this.calendarOptions.events as any).push({
+        title: 'New Event',
+        start: dateStr,
+        end: dateStr
+      })
+    }
+  }
 
+  handleDateClickEvent (event: any) {
+    this.addEvent(event.dateStr);
+    console.log(event);
+  }
 }
